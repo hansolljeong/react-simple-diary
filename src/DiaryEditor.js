4fd333244544a14cd4b0b92020df9fe2c02aaf06
Hansol <losnah24@gmail.com>
2023-01-23 17:19:28 +0900

    입력칸 구조 완성, 스타일링

--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -1,26 +1,26 @@
 import { useState } from "react";
 
 const DiaryEditor = () => {
-  // const [author, setAuthor] = useState("");
-  // const [content, setContent] = useState("");
   const [state, setState] = useState({
     author: "",
     content: "",
+    emotion: 1,
   });
 
   const handleChangeState = (e) => {
-    console.log(e.target.name);
-    console.log(e.target.value);
-
     setState({
       ...state,
       [e.target.name]: e.target.value,
     });
   };
 
+  const handleSubmit = () => {
+    alert("Save Complete");
+  };
+
   return (
     <div className="DiaryEditor">
-      <h2>오늘의 일기</h2>
+      <h2>Today's Diary</h2>
       <div>
         <input
           name="author"
@@ -34,6 +34,23 @@ const DiaryEditor = () => {
           value={state.content}
           onChange={handleChangeState}
         />
+      </div>
+      <div>
+        <span>Today's Emotion Score : </span>
+        <select
+          name="emotion"
+          value={state.emotion}
+          onChange={handleChangeState}
+        >
+          <option value={1}>1</option>
+          <option value={2}>2</option>
+          <option value={3}>3</option>
+          <option value={4}>4</option>
+          <option value={5}>5</option>
+        </select>
+      </div>
+      <div>
+        <button onClick={handleSubmit}>Save</button>
       </div>
     </div>
   );
