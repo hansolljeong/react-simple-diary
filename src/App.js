c0a840e1e1edd407fa72adea41a5dcc140cae97a
Hansol <losnah24@gmail.com>
2023-01-24 15:48:53 +0900

    useEffect Lifecycle Practice

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useRef, useState } from "react";
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
+import Lifecycle from "./Lifecycle";
 
 const App = () => {
   const [data, setData] = useState([]);
@@ -46,6 +47,7 @@ const App = () => {
 
   return (
     <div className="App">
+      <Lifecycle />
       <DiaryEditor onCreate={onCreate} />
       <DiaryList onEdit={onEdit} onRemove={onRemove} diaryList={data} />
     </div>
