80aa47f286cca9dc2d2ec793bc214b7933473691
Hansol <losnah24@gmail.com>
2023-01-24 15:58:43 +0900

    Unmount (using useEffect)

--- src/Lifecycle.js
+++ src/Lifecycle.js
@@ -1,36 +1,24 @@
 import { useEffect, useState } from "react";
 
-const Lifecycle = () => {
-  const [count, setCount] = useState(0);
-  const [text, setText] = useState("");
-
-  // 첫 렌더링 때만 실행
+const UnmountTest = () => {
   useEffect(() => {
     console.log("Mount!");
+    return () => {
+      // Unmount 시점에 실행됨
+      console.log("Unmount!");
+    };
   }, []);
+  return <div>Unmount Testing Component</div>;
+};
 
-  // 렌더링 될 때마다 실행
-  useEffect(() => {
-    console.log("Update!");
-  });
-
-  // dependency array의 요소가 실행될 때만 함수 실행
-  useEffect(() => {
-    console.log(`count is updated : ${count}`);
-  }, [count]);
-  useEffect(() => {
-    console.log(`text is updated : ${text}`);
-  }, [text]);
+const Lifecycle = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  const toggle = () => setIsVisible(!isVisible);
 
   return (
     <div style={{ padding: 20 }}>
-      <div>
-        {count}
-        <button onClick={() => setCount(count + 1)}>+</button>
-      </div>
-      <div>
-        <input value={text} onChange={(e) => setText(e.target.value)} />
-      </div>
+      <button onClick={toggle}>ON/OFF</button>
+      {isVisible && <UnmountTest />}
     </div>
   );
 };
